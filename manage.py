d60c6b2d30119c31f2781b3b63411413860d3d13
Jonathan Joseph <44317651+jonathanjosh31@users.noreply.github.com>
2020-07-10 23:22:36 +0530

    Add files via upload

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fmsproject.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fms_project.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
